567ee4f3e54c0b28253ea6e85504da3571b63d51
Hans Hendrickx <hhlhendrickx@gmail.com>
2020-04-07 14:28:36 +0200

    Ch5 up with sign up

--- MP50/urls.py
+++ MP50/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include # new
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('staticpages.urls')), # new
+
+# User management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+# Local apps
+    path('accounts/', include('users.urls')), # new
+    path('', include('staticpages.urls')),
 ]
